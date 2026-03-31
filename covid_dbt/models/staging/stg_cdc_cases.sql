051b6021489e0d62c1b10168fcc4f2c03d76050b
Alex Balli <ballir2727@gmail.com>
2026-03-31 18:50:59 -0500

    deaths_per_case fact table in dbt marts

--- covid_dbt/models/staging/stg_cdc_cases.sql
+++ covid_dbt/models/staging/stg_cdc_cases.sql
@@ -10,13 +10,33 @@ SELECT
     NULLIF(TRIM("ETHNICITY"), '') AS Ethnicity,
     NULLIF(TRIM("CASE_ONSET_INTERVAL"), '') AS Case_Onset_Interval,
     NULLIF(TRIM("PROCESS"), '') AS Process_Type,
-    NULLIF(TRIM("EXPOSURE_YN"), '') AS Exposure_Status,
+    CASE
+        WHEN NULLIF(TRIM("EXPOSURE_YN"), '') = 'Yes' THEN 'Yes'
+        WHEN NULLIF(TRIM("EXPOSURE_YN"), '') = 'No' THEN 'No'
+        ELSE 'Missing or Unknown'
+    END AS Exposure_YN,
     NULLIF(TRIM("CURRENT_STATUS"), '') AS Case_Status,
     NULLIF(TRIM("SYMPTOM_STATUS"), '') AS Symptom_Status,
-    NULLIF(TRIM("HOSP_YN"), '') AS Hospitalization_Status,
-    NULLIF(TRIM("ICU_YN"), '') AS ICU_Status,
-    NULLIF(TRIM("DEATH_YN"), '') AS Death_Status,
+    CASE
+        WHEN NULLIF(TRIM("HOSP_YN"), '') = 'Yes' THEN 'Yes'
+        WHEN NULLIF(TRIM("HOSP_YN"), '') = 'No' THEN 'No'
+        ELSE 'Missing or Unknown'
+    END AS Hospitalization_YN,
+    CASE
+        WHEN NULLIF(TRIM("ICU_YN"), '') = 'Yes' THEN 'Yes'
+        WHEN NULLIF(TRIM("ICU_YN"), '') = 'No' THEN 'No'
+        ELSE 'Missing or Unknown'
+    END AS ICU_YN,
+    CASE
+        WHEN NULLIF(TRIM("DEATH_YN"), '') = 'Yes' THEN 'Yes'
+        WHEN NULLIF(TRIM("DEATH_YN"), '') = 'No' THEN 'No'
+        ELSE 'Missing or Unknown'
+    END AS Death_YN,
     NULLIF(TRIM("CASE_POSITIVE_SPECIMEN"), '') AS Positive_Specimen_Interval,
-    NULLIF(TRIM("UNDERLYING_CONDITIONS_YN"), '') AS Underlying_Conditions,
+    CASE
+        WHEN NULLIF(TRIM('UNDERLYING_CONDITIONS_YN'), '') = 'Yes' THEN 'Yes'
+        WHEN NULLIF(TRIM('UNDERLYING_CONDITIONS_YN'), '') = 'No' THEN 'No'
+        ELSE NULL
+    END AS Underlying_Conditions_YN,
     NULLIF(TRIM("UNIQUE_KEY"), '') AS Unique_Key
 FROM {{ source('COVID19_DB', 'CDC_RAW_CASES_1') }}
